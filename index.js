b22fc4ee78fb75d546d2e60786157ba3064503ee
ozaki25 <fs65ugumori@yahoo.co.jp>
2020-06-04 18:22:50 +0900

    add screenshot

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-const { chromium } = require('playwright');
+const { chromium, firefox, webkit } = require('playwright');
 
-(async () => {
-  const browser = await chromium.launch({ headless: false, slowMo: 500 });
+const browserList = [chromium, firefox, webkit];
+
+browserList.forEach(async browserItem => {
+  const browser = await browserItem.launch({ headless: false, slowMo: 500 });
   const page = await browser.newPage();
   await page.goto('http://whatsmyuseragent.org/');
-  await page.screenshot({ path: `example.png` });
+  await page.screenshot({
+    path: `screenshots/${browserItem.name()}/${Date.now()}.png`,
+  });
   await browser.close();
-})();
+});
