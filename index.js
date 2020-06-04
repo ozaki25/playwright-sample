3fcc6be54c43e59d91e0cbc2eee6fa2f9bd72337
ozaki25 <fs65ugumori@yahoo.co.jp>
2020-06-04 18:38:46 +0900

    add demo webapp

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const browserList = [chromium, firefox, webkit];
 browserList.forEach(async browserItem => {
   const browser = await browserItem.launch({ headless: false, slowMo: 500 });
   const page = await browser.newPage();
-  await page.goto('http://whatsmyuseragent.org/');
+  await page.goto('http://localhost:8080/');
   await page.screenshot({
     path: `screenshots/${browserItem.name()}/${Date.now()}.png`,
   });
