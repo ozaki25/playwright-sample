594094acd057b3bd921af0fd770d574ed4be26d6
ozaki25 <fs65ugumori@yahoo.co.jp>
2020-06-04 18:53:50 +0900

    update test

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ browserList.forEach(async browserItem => {
   const browser = await browserItem.launch({ headless: false, slowMo: 500 });
   const page = await browser.newPage();
   await page.goto('http://localhost:8080/');
+
+  await page.fill('data-test-id=name', 'ozak25');
+  await page.fill('data-test-id=age', '29');
+  await page.click('data-test-id=submit');
+
   await page.screenshot({
     path: `screenshots/${browserItem.name()}/${Date.now()}.png`,
   });
